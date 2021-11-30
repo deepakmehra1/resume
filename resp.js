81eeee88af11363c4bf0c8c6b88d0d5f0e9a2e43
Deepak Mehra <92205452+deepakmehra1@users.noreply.github.com>
2021-11-30 10:41:27 -0800

    Update resp.js

--- resp.js
+++ resp.js
@@ -1,1 +1,10 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navlist = document.querySelector('.navlist')
 
+
+burger.addEventListener('click',()=>{
+    navlist.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+
+})
